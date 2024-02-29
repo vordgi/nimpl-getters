02afff69ab39aaa4f7b2ac9126006954ad6c650e
Vordgi <sasha2822222@gmail.com>
2024-02-29 22:15:09 +0400

    get-32 server contexts in provider scope

--- package/src/create-server-context.tsx
+++ package/src/create-server-context.tsx
@@ -1,16 +1,26 @@
 import type { ServerContext } from './types';
 import { AsyncLocalStorage } from 'async_hooks';
 import { serverGetterInClientComponentError } from './server-getter-in-client-component-error';
+import React from 'react';
+
+const Enter = ({ storage, value }: { storage: any, value: any }) => {
+    storage.enterWith({ value });
+    return <></>;
+}
 
 function createServerContext<T>(defaultValue?: T): ServerContext<T> {
     serverGetterInClientComponentError('createServerContext');
 
-    const storage = new AsyncLocalStorage<{ value: T }>();
+    const storage: ServerContext<T>['_storage'] = new AsyncLocalStorage<{ value: T }>();
 
     return {
-        Provider: ({ children, value }) => {
-            storage.enterWith({ value });
-            return children;
+        Provider: async ({ children, value }) => {
+            return (
+                <>
+                    <Enter storage={storage} value={value} />
+                    {children}
+                </>
+            )
         },
         Consumer: ({ children }) => {
             const store = storage.getStore();
