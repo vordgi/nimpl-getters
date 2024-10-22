939296ef2b9a6665d9b38f7f6a0b63908bb963c9
Vordgi <sasha2822222@gmail.com>
2024-10-23 00:52:02 +0400

    get-85 update tests

--- tests/base/app/dynamic/error/page.tsx
+++ tests/base/app/dynamic/error/page.tsx
@@ -1,14 +1,12 @@
 import { getPathname } from "@nimpl/getters/get-pathname";
 import { getPageConfig } from "@nimpl/getters/get-page-config";
 import { getParams } from "@nimpl/getters/get-params";
-import { getSearchParams } from "@nimpl/getters/get-search-params";
 import Nav from "../../../components/Nav";
 
 export default function Page() {
     const pathname = getPathname();
     const pageConfig = getPageConfig();
     const params = getParams();
-    const searchParams = getSearchParams({ ignoreDynamicOptionErrors: true });
 
     return (
         <div id="dynamic-error-page">
@@ -17,7 +15,6 @@ export default function Page() {
                 <p id="get-pathname">{pathname}</p>
                 <p id="get-page-config">{JSON.stringify(pageConfig)}</p>
                 <p id="get-params">{JSON.stringify(params)}</p>
-                <p id="get-search-params">{searchParams.toString()}</p>
             </div>
         </div>
     );
